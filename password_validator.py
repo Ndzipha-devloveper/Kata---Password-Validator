0c562e4014a5d38f2025c2911004defa9a47a4dc
Ndzipha.dev <145711212+Ndzipha-devloveper@users.noreply.github.com>
2025-11-04 13:37:35 +0200

    Update password_validator.py
    initially my Output  was{
    Enter your password 
    Password cannot be blank. 
    Enter your password 1234567
    pssword must contain at least one uppercase letter.
    Password must contain at least one lowercase letter.
    Enter your password 
    } so i eplaced valid = True flag with --- [Continue]
     because i wanted to skip the rest of 
     the checks when one check fails

--- password_validator.py
+++ password_validator.py
@@ -5,24 +5,24 @@ def is_password_secure():
     
     if not password:
       print("Password cannot be blank")
-      valid = False
+      continue
       
       #Length of the password
       if not len(password) < 8:
         print("Password must be at least 8 characters long")
-        valid = False
+        continue
         
       if not any(char.isupper) for char in password:
         print("Password must containt at least one uppercase letter")
-        valid = False
+        continue
         
       if not any(char.islower() for char in password:
         print("Password must contain at least one lowercase letter")
-        valid = False
+        continue
         
       if not any(char.isdigit() for char in password:
         print("Password must contain at least one digit")
-        value = False
+        continue
         
         if valid:
           print("Your password is secure")
